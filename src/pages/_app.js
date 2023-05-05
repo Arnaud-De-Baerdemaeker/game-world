59711a50fca4073516b4f479783496d1a07f8662
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-05-05 10:16:35 +0200

    [refactor] Add meta infos

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+/*
+	Game World
+	April 2023
+	Arnaud De Baerdemaeker
+*/
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "./../styles/globals.css";
+
+export default function App({Component, pageProps}) {
+	return(
+		<Component {...pageProps} />
+	);
+};
