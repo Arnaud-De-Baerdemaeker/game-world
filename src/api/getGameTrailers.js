65795f88d05703fc056ccdde92ea5f781bc01f91
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-05-18 19:26:45 +0200

    [feat] Set API call to fetch the trailers of a game based on a slug

---
+++ src/api/getGameTrailers.js
@@ -0,0 +1,15 @@
+/*
+	Game World
+	April 2023
+	Arnaud De Baerdemaeker
+*/
+
+import axiosInstance from "./baseAPICallSettings";
+
+const getGameTrailers = async (slug) => {
+	return axiosInstance.get(
+		`/games/${slug}/movies`
+	);
+};
+
+export {getGameTrailers};
