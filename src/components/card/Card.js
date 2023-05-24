aff00cd868aefe4500b2f9e4b733c53b44bc2ab7
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-05-24 15:32:23 +0200

    [refactor] Make the Card reusable for different situations

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,44 +4,21 @@
 	Arnaud De Baerdemaeker
 */
 
+import Link from "next/link";
+
 const Card = (props) => {
 	return(
-		<article>
-			<div>
-				<img src={props.backgroundImage} alt="" className="" />
-				<h3 data-name={props.slug}>{props.name}</h3>
-			</div>
-			<menu>
-				{/* <li></li> */}
-			</menu>
-			<dl>
-				<dt>Platforms</dt>
-				{props.platforms.length > 0
-					? <>
-						{props.platforms.map(entry => (
-							<dd data-platform={entry.platform.slug}>{entry.platform.name}</dd>
-						))}
-					</>
-					: <dd>TBA</dd>
+		<Link
+			href={{
+				pathname: props.pathname,
+				query: {
+					id: props.id
 				}
-
-				<dt>Release</dt>
-				{props.released
-					? <dd>{props.released}</dd>
-					: <dd>TBA</dd>
-				}
-
-				<dt>Genres</dt>
-				{props.genres.length > 0
-					? <>
-						{props.genres.map(entry => (
-							<dd data-genre={entry.slug}>{entry.name}</dd>
-						))}
-					</>
-					: <dd>TBA</dd>
-				}
-			</dl>
-		</article>
+			}}
+			as={props.as}
+		>
+			{props.children}
+		</Link>
 	);
 };
 
