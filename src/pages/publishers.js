fcc3557c71ac509730cb31d56663ba586ec64990
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-05-31 15:35:03 +0200

    [feat] Add a "load more" function to fetch more publishers

--- src/pages/publishers.js
+++ src/pages/publishers.js
@@ -5,15 +5,25 @@
 */
 
 import Head from "next/head";
+import {useState, useEffect} from "react";
 
 import Menu from "@/components/menu/Menu";
 import List from "@/components/list/List";
 import Card from "@/components/card/Card";
-// import CompaniesList from "@/components/companiesList/CompaniesList";
+import LoadMore from "@/components/loadMore/LoadMore";
 
 import {getPublishers} from "@/api/publishers/getPublishers";
 
 const Publishers = (props) => {
+	const [nextPage, setNextPage] = useState(2);
+	const [moreResults, setMoreResults] = useState([]);
+	const [hasFirstCallMoreResults, setHasFirstCallMoreResults] = useState(null);
+	const [hasFollowingCallsMoreResults, setHasFollowingCallsMoreResults] = useState(null);
+
+	useEffect(() => {
+		props.publishers.next != null ? setHasFirstCallMoreResults(true) : setHasFirstCallMoreResults(false);
+	}, []);
+
 	return(
 		<>
 			<Head>
@@ -32,28 +42,63 @@ const Publishers = (props) => {
 				/>
 			</Head>
 			<Menu />
-			<List title="Publishers">
-				{props.publishers.results.length > 0
-					? props.publishers.results.map(entry => (
-						<Card
-							key={entry.id}
-							id={entry.id}
-							slug={entry.slug}
-							pathname={`/publisher/[slug]`}
-							as={`/publisher/${entry.slug}`}
-						>
-							<div style={{backgroundImage: `url("${entry.image_background}")`}}>
-								<h3>{entry.name}</h3>
-								<dl>
-									<dt>Games count</dt>
-									<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
-								</dl>
-							</div>
-						</Card>
-					))
-					: <p>No results were returned</p>
-				}
-			</List>
+			<main>
+				<List title="Publishers">
+					{props.publishers.results.length > 0
+						? props.publishers.results.map(entry => (
+							<Card
+								key={entry.id}
+								id={entry.id}
+								slug={entry.slug}
+								pathname={`/publisher/[slug]`}
+								as={`/publisher/${entry.slug}`}
+							>
+								<div style={{backgroundImage: `url("${entry.image_background}")`}}>
+									<h3>{entry.name}</h3>
+									<dl>
+										<dt>Games count</dt>
+										<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
+									</dl>
+								</div>
+							</Card>
+						))
+						: <p>No results were returned</p>
+					}
+					{moreResults.length != 0
+						? moreResults.map(entry => (
+							<Card
+								key={entry.id}
+								id={entry.id}
+								slug={entry.slug}
+								pathname={`/publisher/[slug]`}
+								as={`/publisher/${entry.slug}`}
+							>
+								<div style={{backgroundImage: `url("${entry.image_background}")`}}>
+									<h3>{entry.name}</h3>
+									<dl>
+										<dt>Games count</dt>
+										<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
+									</dl>
+								</div>
+							</Card>
+						))
+						: null
+					}
+				</List>
+				<LoadMore
+					nextPage={nextPage}
+					setNextPage={setNextPage}
+					moreResults={moreResults}
+					setMoreResults={setMoreResults}
+					setHasFirstCallMoreResults={setHasFirstCallMoreResults}
+					setHasFollowingCallsMoreResults={setHasFollowingCallsMoreResults}
+					apiCall={getPublishers}
+					next={hasFirstCallMoreResults || hasFollowingCallsMoreResults
+						? true
+						: false
+					}
+				/>
+			</main>
 		</>
 	);
 };
