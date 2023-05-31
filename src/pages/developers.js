fb4e1770ebfef8616b2979ea41777b2440665390
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-05-31 15:31:43 +0200

    [refactor] Apply modifications
    - Add a "load more" button
    - Use states to control the "load more" button's behavior

--- src/pages/developers.js
+++ src/pages/developers.js
@@ -5,14 +5,25 @@
 */
 
 import Head from "next/head";
+import {useState, useEffect} from "react";
 
 import Menu from "@/components/menu/Menu";
 import List from "@/components/list/List";
 import Card from "@/components/card/Card";
+import LoadMore from "@/components/loadMore/LoadMore";
 
 import {getDevelopers} from "@/api/developers/getDevelopers";
 
 const Developers = (props) => {
+	const [nextPage, setNextPage] = useState(2);
+	const [moreResults, setMoreResults] = useState([]);
+	const [hasFirstCallMoreResults, setHasFirstCallMoreResults] = useState(null);
+	const [hasFollowingCallsMoreResults, setHasFollowingCallsMoreResults] = useState(null);
+
+	useEffect(() => {
+		props.developers.next != null ? setHasFirstCallMoreResults(true) : setHasFirstCallMoreResults(false);
+	}, []);
+
 	return(
 		<>
 			<Head>
@@ -31,28 +42,63 @@ const Developers = (props) => {
 				/>
 			</Head>
 			<Menu />
-			<List title="Developers">
-				{props.developers.results.length > 0
-					? props.developers.results.map(entry => (
-						<Card
-							key={entry.id}
-							id={entry.id}
-							slug={entry.slug}
-							pathname={`/developer/[slug]`}
-							as={`/developer/${entry.slug}`}
-						>
-							<div style={{backgroundImage: `url("${entry.image_background}")`}}>
-								<h3>{entry.name}</h3>
-								<dl>
-									<dt>Games count</dt>
-									<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
-								</dl>
-							</div>
-						</Card>
-					))
-					: <p>No results were returned</p>
-				}
-			</List>
+			<main>
+				<List title="Developers">
+					{props.developers.results.length > 0
+						? props.developers.results.map(entry => (
+							<Card
+								key={entry.id}
+								id={entry.id}
+								slug={entry.slug}
+								pathname={`/developer/[slug]`}
+								as={`/developer/${entry.slug}`}
+							>
+								<div style={{backgroundImage: `url("${entry.image_background}")`}}>
+									<h3>{entry.name}</h3>
+									<dl>
+										<dt>Games count</dt>
+										<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
+									</dl>
+								</div>
+							</Card>
+						))
+						: <p>No results were returned</p>
+					}
+					{moreResults.length != 0
+						? moreResults.map(entry => (
+							<Card
+								key={entry.id}
+								id={entry.id}
+								slug={entry.slug}
+								pathname={`/publisher/[slug]`}
+								as={`/publisher/${entry.slug}`}
+							>
+								<div style={{backgroundImage: `url("${entry.image_background}")`}}>
+									<h3>{entry.name}</h3>
+									<dl>
+										<dt>Games count</dt>
+										<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
+									</dl>
+								</div>
+							</Card>
+						))
+						: null
+					}
+				</List>
+				<LoadMore
+					nextPage={nextPage}
+					setNextPage={setNextPage}
+					moreResults={moreResults}
+					setMoreResults={setMoreResults}
+					setHasFirstCallMoreResults={setHasFirstCallMoreResults}
+					setHasFollowingCallsMoreResults={setHasFollowingCallsMoreResults}
+					apiCall={getDevelopers}
+					next={hasFirstCallMoreResults || hasFollowingCallsMoreResults
+						? true
+						: false
+					}
+				/>
+			</main>
 		</>
 	);
 };
@@ -60,7 +106,7 @@ const Developers = (props) => {
 export default Developers;
 
 const getStaticProps = async () => {
-	const developersRequest = await getDevelopers();
+	const developersRequest = await getDevelopers({});
 	const developersResponse = developersRequest.data;
 
 	return {
