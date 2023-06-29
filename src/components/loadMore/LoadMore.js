13b24ddeb3ff8450b2d58cec3e4824a413d3e978
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-06-29 10:28:49 +0200

    [feat] Set a "load more" component
    On click, it will load more content following the parameters it will receive.

---
+++ src/components/loadMore/LoadMore.js
@@ -0,0 +1,61 @@
+/*
+	Game World
+	April 2023
+	Arnaud De Baerdemaeker
+*/
+
+import Button from "@/components/button/Button";
+
+const LoadMore = (props) => {
+	if(props.resultsCount == 0) {
+		return(
+			<p>Your research returned no results</p>
+		);
+	}
+	else if(props.moreResults.length == 0 && props.next == false) {
+		return(
+			<p>End of the results</p>
+		);
+	}
+	else {
+		return(
+			<Button
+				buttonType="submit"
+				buttonAction={
+					async () => {
+						const parameters = {
+							slug: props.slug ? props.slug : null,
+							query: props.searchQuery ? props.searchQuery : null,
+							nextPage: props.nextPage ? props.nextPage : null
+						};
+
+						const loadMore = await props.apiCall(parameters);
+
+						if(props.moreResults.length == 0) {
+							props.setMoreResults(loadMore.data.results);
+						}
+						else {
+							props.setMoreResults(props.moreResults.concat(loadMore.data.results));
+						}
+
+						props.setNextPage(props.nextPage + 1);
+
+						if(loadMore.data.next != null) {
+							props.setHasFollowingCallsMoreResults(true);
+							props.setHasFirstCallMoreResults(null);
+						}
+						else {
+							props.setHasFollowingCallsMoreResults(false);
+							props.setHasFirstCallMoreResults(null);
+						}
+					}
+				}
+				buttonClass=""
+			>
+				Show more results
+			</Button>
+		);
+	}
+};
+
+export default LoadMore;
