35513ff28ae92bde4544bf1c4c14798b8dc8d639
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-06-29 10:37:12 +0200

    [refactor] Add the "load more" function and the place to show the result

--- src/pages/platforms.js
+++ src/pages/platforms.js
@@ -5,15 +5,25 @@
 */
 
 import Head from "next/head";
+import {useState, useEffect} from "react";
 
 import Menu from "@/components/menu/Menu";
 import List from "@/components/list/List";
 import Card from "@/components/card/Card";
-// import PlatformsList from "@/components/platformsList/platformsList";
+import LoadMore from "@/components/loadMore/LoadMore";
 
 import {getPlatforms} from "@/api/platforms/getPlatforms";
 
 const Platforms = (props) => {
+	const [nextPage, setNextPage] = useState(2);
+	const [moreResults, setMoreResults] = useState([]);
+	const [hasFirstCallMoreResults, setHasFirstCallMoreResults] = useState(null);
+	const [hasFollowingCallsMoreResults, setHasFollowingCallsMoreResults] = useState(null);
+
+	useEffect(() => {
+		props.platforms.next != null ? setHasFirstCallMoreResults(true) : setHasFirstCallMoreResults(false);
+	}, []);
+
 	return(
 		<>
 			<Head>
@@ -32,34 +42,75 @@ const Platforms = (props) => {
 				/>
 			</Head>
 			<Menu />
-			<List title="Platforms">
-				{props.platforms.results.length > 0
-					? props.platforms.results.map(entry => (
-						<Card
-							key={entry.id}
-							id={entry.id}
-							slug={entry.slug}
-							pathname="/platform/[slug]"
-							as={`/platform/${entry.slug}`}
-						>
-							<div style={{background: `url("${entry.image_background}")`}}>
-								<h3>{entry.name}</h3>
-								<dl>
-									<dt>Games count</dt>
-									<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
+			<main>
+				<List title="Platforms">
+					{props.platforms.results.length > 0
+						? props.platforms.results.map(entry => (
+							<Card
+								key={entry.id}
+								id={entry.id}
+								slug={entry.slug}
+								pathname="/platform/[slug]"
+								as={`/platform/${entry.slug}`}
+							>
+								<div style={{background: `url("${entry.image_background}")`}}>
+									<h3>{entry.name}</h3>
+									<dl>
+										<dt>Games count</dt>
+										<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
 
-									<dt>Start year</dt>
-									<dd>{entry.year_start ? props.year_start : "N/A"}</dd>
+										<dt>Start year</dt>
+										<dd>{entry.year_start ? props.year_start : "N/A"}</dd>
 
-									<dt>End year</dt>
-									<dd>{props.year_end ? props.year_end : "N/A"}</dd>
-								</dl>
-							</div>
-						</Card>
-					))
-					: <p>No results were returned</p>
-				}
-			</List>
+										<dt>End year</dt>
+										<dd>{props.year_end ? props.year_end : "N/A"}</dd>
+									</dl>
+								</div>
+							</Card>
+						))
+						: <p>No results were returned</p>
+					}
+					{moreResults.length != 0
+						? moreResults.map(entry => (
+							<Card
+								key={entry.id}
+								id={entry.id}
+								slug={entry.slug}
+								pathname="/platform/[slug]"
+								as={`/platform/${entry.slug}`}
+							>
+								<div style={{background: `url("${entry.image_background}")`}}>
+									<h3>{entry.name}</h3>
+									<dl>
+										<dt>Games count</dt>
+										<dd>{entry.games_count ? entry.games_count : "N/A"}</dd>
+
+										<dt>Start year</dt>
+										<dd>{entry.year_start ? props.year_start : "N/A"}</dd>
+
+										<dt>End year</dt>
+										<dd>{props.year_end ? props.year_end : "N/A"}</dd>
+									</dl>
+								</div>
+							</Card>
+						))
+						: null
+					}
+				</List>
+				<LoadMore
+					nextPage={nextPage}
+					setNextPage={setNextPage}
+					moreResults={moreResults}
+					setMoreResults={setMoreResults}
+					setHasFirstCallMoreResults={setHasFirstCallMoreResults}
+					setHasFollowingCallsMoreResults={setHasFollowingCallsMoreResults}
+					apiCall={getPlatforms}
+					next={hasFirstCallMoreResults || hasFollowingCallsMoreResults
+						? true
+						: false
+					}
+				/>
+			</main>
 		</>
 	);
 };
