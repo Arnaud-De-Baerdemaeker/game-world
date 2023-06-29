1f72dc94b40c671c593d71017c794a70aeb4d301
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-06-29 10:39:57 +0200

    [feat] Set the search and reset functions
    Send the results in their respective components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,50 @@ import Head from "next/head";
 import {useState, useEffect} from "react";
 
 import Menu from "@/components/menu/Menu";
-import List from "@/components/list/List";
-import Card from "@/components/card/Card";
-import LoadMore from "@/components/loadMore/LoadMore";
+import SearchField from "@/components/searchField/SearchField";
+import Button from "@/components/button/Button";
+import SearchResults from "@/components/searchResults/SearchResults";
+import LatestReleasesResults from "@/components/latestReleasesResults/LatestReleasesResults";
 
 import {getLatestReleases} from "@/api/games/getLatestReleases";
+import {searchGames} from "@/api/games/searchGames";
 
-import styles from "./Home.module.css";
+// import styles from "./Home.module.css";
 
 const Home = (props) => {
-	const [nextPage, setNextPage] = useState(2);
-	const [moreResults, setMoreResults] = useState([]);
+	const [searchQuery, setSearchQuery] = useState(null);
+	const [searchResults, setSearchResults] = useState(null);
+	const [isSearchInUse, setIsSearchInUse] = useState(false);
 	const [hasFirstCallMoreResults, setHasFirstCallMoreResults] = useState(null);
 	const [hasFollowingCallsMoreResults, setHasFollowingCallsMoreResults] = useState(null);
+
+	const handleSearchQuery = async (event) => {
+		event.preventDefault();
+
+		const query = event.target[0].value;
+		setSearchQuery(query);
+
+		await searchGames({query})
+		.then(results => {
+			setIsSearchInUse(true);
+			setSearchResults(results.data);
+
+			if(results.data.next != null) {
+				setHasFollowingCallsMoreResults(true);
+				setHasFirstCallMoreResults(null);
+			}
+			else {
+				setHasFollowingCallsMoreResults(false);
+				setHasFirstCallMoreResults(null);
+			}
+		});
+	};
+
+	const resetAll = () => {
+		setIsSearchInUse(false);
+		setSearchQuery(null);
+		setSearchResults(null);
+	}
 
 	useEffect(() => {
 		props.latestReleases.next != null ? setHasFirstCallMoreResults(true) : setHasFirstCallMoreResults(false);
@@ -45,151 +76,35 @@ const Home = (props) => {
 			</Head>
 			<Menu />
 			<main>
-				<List title="Latest releases">
-					{props.latestReleases.results.length > 0
-						? props.latestReleases.results.map(entry => (
-							<Card
-								key={entry.id}
-								id={entry.id}
-								slug={entry.slug}
-								pathname={`/game/[slug]`}
-								as={`/game/${entry.slug}`}
-								dominantColor={entry.dominant_color}
-								saturatedColor={entry.saturated_color}
-								shortScreenshots={entry.short_screenshorts}
-								tags={entry.tags}
-							>
-								<article>
-									<div>
-										<img
-											src={entry.background_image}
-											alt=""
-											className=""
-										/>
-										<h3 data-name={entry.slug}>{entry.name}</h3>
-									</div>
-									<menu></menu>
-									<dl>
-										<dt>Platforms</dt>
-										{entry.platforms
-											? <>
-												{entry.platforms.map(item => (
-													<dd
-														key={item.platform.id}
-														data-platform={item.platform.slug}
-													>
-														{item.platform.name}
-													</dd>
-												))}
-											</>
-											: <dd>N/A</dd>
-										}
-
-										<dt>Release</dt>
-										{entry.released
-											? <dd>{entry.released}</dd>
-											: <dd>N/A</dd>
-										}
-
-										<dt>Genres</dt>
-										{entry.genres
-											? <>
-												{entry.genres.map(item => (
-													<dd
-														key={item.id}
-														data-genre={item.slug}
-													>
-														{item.name}
-													</dd>
-												))}
-											</>
-											: <dd>N/A</dd>
-										}
-									</dl>
-								</article>
-							</Card>
-						))
-						: <p>No results were returned</p>
-					}
-					{moreResults.length != 0
-						? moreResults.map(entry => (
-							<Card
-								key={entry.id}
-								id={entry.id}
-								slug={entry.slug}
-								pathname={`/game/[slug]`}
-								as={`/game/${entry.slug}`}
-								dominantColor={entry.dominant_color}
-								saturatedColor={entry.saturated_color}
-								shortScreenshots={entry.short_screenshorts}
-								tags={entry.tags}
-							>
-								<article>
-									<div>
-										<img
-											src={entry.background_image}
-											alt=""
-											className=""
-										/>
-										<h3 data-name={entry.slug}>{entry.name}</h3>
-									</div>
-									<menu></menu>
-									<dl>
-										<dt>Platforms</dt>
-										{entry.platforms
-											? <>
-												{entry.platforms.map(item => (
-													<dd
-														key={item.platform.id}
-														data-platform={item.platform.slug}
-													>
-														{item.platform.name}
-													</dd>
-												))}
-											</>
-											: <dd>N/A</dd>
-										}
-
-										<dt>Release</dt>
-										{entry.released
-											? <dd>{entry.released}</dd>
-											: <dd>N/A</dd>
-										}
-
-										<dt>Genres</dt>
-										{entry.genres
-											? <>
-												{entry.genres.map(item => (
-													<dd
-														key={item.id}
-														data-genre={item.slug}
-													>
-														{item.name}
-													</dd>
-												))}
-											</>
-											: <dd>N/A</dd>
-										}
-									</dl>
-								</article>
-							</Card>
-						))
-						: null
-					}
-				</List>
-				<LoadMore
-					nextPage={nextPage}
-					setNextPage={setNextPage}
-					moreResults={moreResults}
-					setMoreResults={setMoreResults}
-					setHasFirstCallMoreResults={setHasFirstCallMoreResults}
-					setHasFollowingCallsMoreResults={setHasFollowingCallsMoreResults}
-					apiCall={getLatestReleases}
-					next={hasFirstCallMoreResults || hasFollowingCallsMoreResults
-						? true
-						: false
+				<SearchField
+					formAction={handleSearchQuery}
+					buttonReset={
+						<Button
+							buttonType="reset"
+							buttonClass=""
+							buttonAction={resetAll}
+						>
+							Clear search
+						</Button>
 					}
 				/>
+				{isSearchInUse
+					? <SearchResults
+						searchQuery={searchQuery}
+						searchResults={searchResults}
+						hasFirstCallMoreResults={hasFirstCallMoreResults}
+						setHasFirstCallMoreResults={setHasFirstCallMoreResults}
+						hasFollowingCallsMoreResults={hasFollowingCallsMoreResults}
+						setHasFollowingCallsMoreResults={setHasFollowingCallsMoreResults}
+					/>
+					: <LatestReleasesResults
+						latestReleases={props.latestReleases}
+						hasFirstCallMoreResults={hasFirstCallMoreResults}
+						setHasFirstCallMoreResults={setHasFirstCallMoreResults}
+						hasFollowingCallsMoreResults={hasFollowingCallsMoreResults}
+						setHasFollowingCallsMoreResults={setHasFollowingCallsMoreResults}
+					/>
+				}
 			</main>
 		</>
 	);
