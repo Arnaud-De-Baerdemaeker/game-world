955f3e8449c3bfa9f10fad4ce567d5906186c781
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-06-29 10:48:34 +0200

    [feat] Set the "load more" function and the location to show the result

--- src/pages/developer/[slug].js
+++ src/pages/developer/[slug].js
@@ -5,14 +5,25 @@
 */
 
 import Head from "next/head";
+import {useState, useEffect} from "react";
 
 import Menu from "@/components/menu/Menu";
 import Card from "@/components/card/Card";
+import LoadMore from "@/components/loadMore/LoadMore";
 
 import {getDeveloperDetails} from "@/api/developers/getDeveloperDetails";
 import {getGamesFromDeveloper} from "@/api/games/getGamesFromDeveloper";
 
 const Developer = (props) => {
+	const [nextPage, setNextPage] = useState(2);
+	const [moreResults, setMoreResults] = useState([]);
+	const [hasFirstCallMoreResults, setHasFirstCallMoreResults] = useState(null);
+	const [hasFollowingCallsMoreResults, setHasFollowingCallsMoreResults] = useState(null);
+
+	useEffect(() => {
+		props.gamesFromDeveloper.next != null ? setHasFirstCallMoreResults(true) : setHasFirstCallMoreResults(false);
+	}, []);
+
 	return(
 		<>
 			<Head>
@@ -113,6 +124,85 @@ const Developer = (props) => {
 					</section>
 					: <p>No results were returned</p>
 				}
+				{moreResults.length != 0
+					? moreResults.map(entry => (
+						<Card
+							key={entry.id}
+							id={entry.id}
+							slug={entry.slug}
+							pathname={`/game/[slug]`}
+							as={`/game/${entry.slug}`}
+							dominantColor={entry.dominant_color}
+							saturatedColor={entry.saturated_color}
+							shortScreenshots={entry.short_screenshorts}
+							tags={entry.tags}
+						>
+							<article>
+								<div>
+									<img
+										src={entry.background_image}
+										alt=""
+										className=""
+									/>
+									<h3 data-name={entry.slug}>{entry.name}</h3>
+								</div>
+								<menu></menu>
+								<dl>
+									<dt>Platforms</dt>
+									{entry.platforms
+										? <>
+											{entry.platforms.map(item => (
+												<dd
+													key={item.platform.id}
+													data-platform={item.platform.slug}
+												>
+													{item.platform.name}
+												</dd>
+											))}
+										</>
+										: <dd>N/A</dd>
+									}
+	
+									<dt>Release</dt>
+									{entry.released
+										? <dd>{entry.released}</dd>
+										: <dd>N/A</dd>
+									}
+	
+									<dt>Genres</dt>
+									{entry.genres
+										? <>
+											{entry.genres.map(item => (
+												<dd
+													key={item.id}
+													data-genre={item.slug}
+												>
+													{item.name}
+												</dd>
+											))}
+										</>
+										: <dd>N/A</dd>
+									}
+								</dl>
+							</article>
+						</Card>
+					))
+					: null
+				}
+				<LoadMore
+					nextPage={nextPage}
+					setNextPage={setNextPage}
+					moreResults={moreResults}
+					setMoreResults={setMoreResults}
+					setHasFirstCallMoreResults={setHasFirstCallMoreResults}
+					setHasFollowingCallsMoreResults={setHasFollowingCallsMoreResults}
+					slug={props.developerDetails.slug}
+					apiCall={getGamesFromDeveloper}
+					next={hasFirstCallMoreResults || hasFollowingCallsMoreResults
+						? true
+						: false
+					}
+				/>
 			</main>
 		</>
 	);
@@ -124,7 +214,7 @@ const getServerSideProps = async (context) => {
 	const developerDetailsRequest = await getDeveloperDetails(context.query.slug);
 	const developerDetailsResponse = developerDetailsRequest.data;
 
-	const gamesFromDeveloperRequest = await getGamesFromDeveloper(context.query.slug);
+	const gamesFromDeveloperRequest = await getGamesFromDeveloper({slug: context.query.slug});
 	const gamesFromDeveloperResponse = gamesFromDeveloperRequest.data;
 
 	return {
