aec65869fd93adab341d7c7ca2876efd95b32e8f
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-07-11 14:29:55 +0200

    [feat] Set the storage using IndexedDB
    - Move the link surrounding the card to its own button

--- src/components/cards/GameCard.js
+++ src/components/cards/GameCard.js
@@ -6,72 +6,206 @@
 
 import Link from "next/link";
 
+import Button from "@/components/button/Button";
+
 const GameCard = (props) => {
-	return(
-		<Link
-			href={{
-				pathname: props.pathname,
-				query: {
-					id: props.id
+	// Define the data to add to the database
+	const newEntry = {
+		id: props.id,
+		slug: props.slug,
+		as: props.as,
+		imageSrc: props.imageSrc,
+		imageAlt: props.imageAlt,
+		gameName: props.gameName,
+		gamePlatforms: props.gamePlatforms,
+		gameRelease: props.gameRelease,
+		gameGenres: props.gameGenres
+	};
+
+	const addToLibrary = () => {
+		const openDatabase = window.indexedDB.open("game-world-database", 1);
+		openDatabase.onsuccess = (event) => {
+			let openedDatabase = event.target.result;
+
+			// Open a transaction and access the Library object store
+			let stores = openedDatabase.transaction(["Games library", "Games wishlist"], "readwrite");
+			let gamesLibraryStore = stores.objectStore("Games library");
+			let gamesWishlistStore = stores.objectStore("Games wishlist");
+
+			// Check if the entry is already in the database
+			let checkGamesLibraryEntry = gamesLibraryStore.count(props.id);
+			checkGamesLibraryEntry.onsuccess = (event) => {
+				if(event.target.result === 0) {
+					// Add a new entry to the object store
+					let objectStoreAdd = gamesLibraryStore.add(newEntry);
+					objectStoreAdd.onsuccess = (event) => {
+						// Check if the same entry is in the wishlist
+						let checkGamesWishlistEntry = gamesWishlistStore.count(props.id);
+						checkGamesWishlistEntry.onsuccess = (event) => {
+							if(event.target.result !== 0) {
+								let objectStoreDelete = gamesWishlistStore.delete(props.id);
+								objectStoreDelete.onsuccess = (event) => {
+									// TODO: Add modal to indicate the addition worked
+									// TODO: Signify the item has been removed from the wishlist if added in the library
+								};
+								objectStoreDelete.onerror = (event) => {
+									// TODO: Signify the operation to remove from the wishlist failed
+								};
+							}
+						};
+					};
 				}
-			}}
-			as={props.as}
-		>
-			<article>
-				<div>
-					<figure>
-						<img
-							src={props.imageSrc}
-							alt={props.imageAlt}
-							className={props.imageClass}
-						/>
-					</figure>
-					<h3 data-name={props.slug}>{props.gameName}</h3>
-				</div>
+				else {
+					// Remove the entry from the database
+					let objectStoreDelete = gamesLibraryStore.delete(props.id);
+					objectStoreDelete.onsuccess = (event) => {
+						// TODO: Add modal to indicate the addition failed
+					};
+				}
+			};
+		};
+		openDatabase.onerror = (event) => {
+			// TODO: Think of a way to let know the connection to the database failed
+		};
+	};
 
-				<menu>
-					{/* <li></li> */}
-				</menu>
+	const addToWishlist = () => {
+		const openDatabase = window.indexedDB.open("game-world-database", 1);
+		openDatabase.onsuccess = (event) => {
+			let openedDatabase = event.target.result;
 
-				<dl>
-					<dt>Platforms</dt>
-					{props.gamePlatforms.length > 0
-						? <>
-							{props.gamePlatforms.map(item => (
-								<dd
-									key={item.platform.id}
-									data-platform={item.platform.slug}
-								>
-									{item.platform.name}
-								</dd>
-							))}
-						</>
-						: <dd>N/A</dd>
-					}
+			// Open a transaction and access the Library object store
+			let stores = openedDatabase.transaction(["Games wishlist", "Games library"], "readwrite");
+			let gamesWishlistStore = stores.objectStore("Games wishlist");
+			let gamesLibraryStore = stores.objectStore("Games library");
 
-					<dt>Release</dt>
-					{props.gameRelease
-						? <dd>{props.gameRelease}</dd>
-						: <dd>N/A</dd>
-					}
+			// Check if the entry is already in the database
+			let checkGamesWishlistEntry = gamesWishlistStore.count(props.id);
+			checkGamesWishlistEntry.onsuccess = (event) => {
+				if(event.target.result === 0) {
+					// Add a new entry to the object store
+					let objectStoreAdd = gamesWishlistStore.add(newEntry);
+					objectStoreAdd.onsuccess = (event) => {
+						// Check if the same entry is in the database
+						let checkGamesLibraryEntry = gamesLibraryStore.count(props.id);
+						checkGamesLibraryEntry.onsuccess = (event) => {
+							if(event.target.result !== 0) {
+								let objectStoreDelete = gamesLibraryStore.delete(props.id);
+								objectStoreDelete.onsuccess = (event) => {
+									// TODO: Add modal to indicate the addition worked
+									// TODO: Signify the item has also been removed from the library if it was in
+								};
+								objectStoreDelete.onerror = (event) => {
+									// TODO: Signify the operation to remove from the wishlist failed
+								};
+							}
+						};
+					};
+				}
+				else {
+					// Remove the entry from the database
+					let objectStoreDelete = gamesWishlistStore.delete(props.id);
+					objectStoreDelete.onsuccess = (event) => {
+						// TODO: Add modal to indicate the addition failed
+					};
+					objectStoreDelete.onerror = (event) => {
+						// TODO: Add modal to indicate the addition failed
+					};
+				}
+			};
+		};
+		openDatabase.onerror = (event) => {
+			// TODO: Think of a way to let know the connection to the database failed
+		};
+	}
 
-					<dt>Genres</dt>
-					{props.gameGenres.length > 0
-						? <>
-							{props.gameGenres.map(item => (
-								<dd
-									key={item.id}
-									data-genre={item.slug}
-								>
-									{item.name}
-								</dd>
-							))}
-						</>
-						: <dd>N/A</dd>
-					}
-				</dl>
-			</article>
-		</Link>
+	return(
+		<article>
+			<div>
+				<figure>
+					<img
+						src={props.imageSrc}
+						alt={props.imageAlt}
+						className={props.imageClass}
+					/>
+				</figure>
+				<h3 data-name={props.slug}>{props.gameName}</h3>
+			</div>
+
+			<menu>
+				<ul>
+					<li>
+						<Link
+							href={{
+								pathname: props.pathname,
+								query: {
+									id: props.id
+								}
+							}}
+							as={props.as}
+						>
+							Voir
+						</Link>
+					</li>
+					<li>
+						<Button
+							buttonType="button"
+							buttonAction={addToLibrary}
+							buttonClass=""
+						>
+							Library
+						</Button>
+					</li>
+					<li>
+						<Button
+							buttonType="button"
+							buttonAction={addToWishlist}
+							buttonClass=""
+						>
+							Wishlist
+						</Button>
+					</li>
+				</ul>
+			</menu>
+
+			<dl>
+				<dt>Platforms</dt>
+				{props.gamePlatforms.length > 0
+					? <>
+						{props.gamePlatforms.map(item => (
+							<dd
+								key={item.platform.id}
+								data-platform={item.platform.slug}
+							>
+								{item.platform.name}
+							</dd>
+						))}
+					</>
+					: <dd>N/A</dd>
+				}
+
+				<dt>Release</dt>
+				{props.gameRelease
+					? <dd>{props.gameRelease}</dd>
+					: <dd>N/A</dd>
+				}
+
+				<dt>Genres</dt>
+				{props.gameGenres.length > 0
+					? <>
+						{props.gameGenres.map(item => (
+							<dd
+								key={item.id}
+								data-genre={item.slug}
+							>
+								{item.name}
+							</dd>
+						))}
+					</>
+					: <dd>N/A</dd>
+				}
+			</dl>
+		</article>
 	);
 };
 
