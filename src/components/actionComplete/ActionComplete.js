11c9d21d6a1fd78217fc2a51bc439abbce81e28d
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-08-04 16:01:36 +0200

    [refactor] Modify SVG settings

--- src/components/actionComplete/ActionComplete.js
+++ src/components/actionComplete/ActionComplete.js
@@ -6,6 +6,7 @@
 
 import SVG from "@/components/svg/SVG";
 
+import actionCompleteStyles from "@/components/actionComplete/ActionComplete.module.scss";
 import svgStyles from "@/components/svg/SVG.module.scss";
 
 const ActionComplete = (props) => {
@@ -13,10 +14,10 @@ const ActionComplete = (props) => {
 		<div className={actionCompleteStyles.actionComplete}>
 			<figure className={actionCompleteStyles.actionComplete__icon}>
 				<SVG
-					svgViewbox="0 0 50 50"
+					svgViewbox="-5 -5 60 60"
 					svgCoordinates={
 						<path
-							d="M44 15.5L25.7071 33.7929C25.3166 34.1834 24.6834 34.1834 24.2929 33.7929L15 24.5"
+							d="M39.5 15.5L21.2071 33.7929C20.8166 34.1834 20.1834 34.1834 19.7929 33.7929L10.5 24.5"
 							strokeLinecap="round"
 						/>
 					}
@@ -28,4 +29,4 @@ const ActionComplete = (props) => {
 	);
 };
 
-export default Popup;
+export default ActionComplete;
