b0e8a47eb575deb95f2875961013ef016097c50e
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-08-04 11:52:15 +0200

    [add] Set the class names

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,19 +6,20 @@
 
 import Image from "next/image";
 
+import styles from "@/components/header/Header.module.scss";
+
 const Header = (props) => {
 	return(
-		<header>
-			<hgroup>
+		<header className={styles.header}>
+			<hgroup className={styles.header__titles}>
 				<h1>{props.mainTitle}</h1>
 				<p>{props.subTitle}</p>
 			</hgroup>
-			<figure>
+			<figure className={styles.header__wallpaper}>
 				<Image
 					src={props.imageSrc}
 					alt={props.imageAlt}
 					priority={true}
-					className={props.imageClass}
 				/>
 			</figure>
 		</header>
