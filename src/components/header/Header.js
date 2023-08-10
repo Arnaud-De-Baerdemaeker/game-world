2e7864ec7676955d08d8473a998e9aa98fb0cc61
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-08-10 14:58:42 +0200

    [refactor] Set the background via CSS

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,24 +4,15 @@
 	Arnaud De Baerdemaeker
 */
 
-import Image from "next/image";
-
-import styles from "@/components/header/Header.module.scss";
+import headerStyles from "@/components/header/Header.module.scss";
 
 const Header = (props) => {
 	return(
-		<header className={styles.header}>
-			<hgroup className={styles.header__titles}>
+		<header className={headerStyles.header}>
+			<hgroup className={headerStyles.header__titles}>
 				<h1>{props.mainTitle}</h1>
 				<p>{props.subTitle}</p>
 			</hgroup>
-			<figure className={styles.header__wallpaper}>
-				<Image
-					src={props.imageSrc}
-					alt={props.imageAlt}
-					priority={true}
-				/>
-			</figure>
 		</header>
 	);
 };
