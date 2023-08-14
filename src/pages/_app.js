19eab5bfec4b2cac336184468beef70ee5033b2b
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-08-14 11:00:30 +0200

    [feat] Load the fonts from Google

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,27 @@
 	Arnaud De Baerdemaeker
 */
 
+import {Audiowide, Jost} from "next/font/google";
+
 import "@/styles/globals.scss";
 import "@/styles/variables.scss";
 
+const audiowide = Audiowide({
+	subsets: ["latin"],
+	weight: "400",
+	variable: "--audiowide"
+});
+
+const jost = Jost({
+	subsets: ["latin"],
+	fallback: ["system-ui", "arial"],
+	variable: "--jost"
+});
+
 export default function App({Component, pageProps}) {
 	return(
-		<Component {...pageProps} />
+		<div className={`fonts ${jost.variable} ${audiowide.variable}`}>
+			<Component {...pageProps} />
+		</div>
 	);
 };
