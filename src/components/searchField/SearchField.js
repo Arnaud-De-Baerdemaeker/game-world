04ccc3791a6557b60284806f54303a2a6217e1d2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-08-30 14:24:42 +0200

    [refactor] Reorganize the form's elements

--- src/components/searchField/SearchField.js
+++ src/components/searchField/SearchField.js
@@ -6,28 +6,50 @@
 
 import Button from "@/components/button/Button";
 
+import searchFieldStyles from "./SearchField.module.scss";
+import buttonStyles from "../button/Button.module.scss";
+
 const SearchField = (props) => {
 	return(
 		<>
 			<form
 				id="searchForm"
 				onSubmit={props.formAction}
+				className={searchFieldStyles.searchField}
 			>
-				<label htmlFor="searchField">Search for a game</label>
-				<input
-					type="search"
-					id="searchField"
-					name="search"
-					placeholder="God of War, The Legend of Zelda, Halo..."
-					required
-				/>
-				<Button
-					buttonType="submit"
-					buttonClass=""
-				>
-					Search
-				</Button>
-				{props.buttonReset}
+				<div className={searchFieldStyles.searchField__container}>
+					<div className={searchFieldStyles.searchField__field}>
+						<label
+							htmlFor="searchField"
+							className={searchFieldStyles.searchField__label}
+						>
+							Search for a game
+						</label>
+						<input
+							type="search"
+							id="searchField"
+							name="search"
+							placeholder="God of War, The Legend of Zelda, Halo..."
+							required
+							className={searchFieldStyles.searchField__input}
+						/>
+					</div>
+					<div className={searchFieldStyles.searchField__buttons}>
+						<Button
+							buttonType="submit"
+							buttonClass={buttonStyles.button__submit}
+						>
+							Search
+						</Button>
+						<Button
+							buttonType="reset"
+							buttonClass={buttonStyles.button__clear}
+							buttonAction={props.resetAll}
+						>
+							Clear
+						</Button>
+					</div>
+				</div>
 			</form>
 		</>
 	);
