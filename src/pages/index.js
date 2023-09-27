16db2790f76eed8431cfbdaa25d38ff0555fa7fa
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-09-27 14:33:24 +0200

    [refactor] Rework the component
    - Use the custom hook to handle the opening/closing of the menu
    - Block the database's logic code if the browser doesn't support IndexedDB

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@
 import Head from "next/head";
 import {useState, useEffect} from "react";
 
+import useToggler from "@/hooks/useToggler";
+
 import Menu from "@/components/menu/Menu";
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
@@ -21,12 +23,12 @@ import {searchGames} from "@/api/games/searchGames";
 import homeStyles from "./Home.module.scss";
 
 const Home = (props) => {
+	const [isMenuOpen, toggleMenu] = useToggler(false);
 	const [searchQuery, setSearchQuery] = useState(null);
 	const [searchResults, setSearchResults] = useState(null);
 	const [isSearchInUse, setIsSearchInUse] = useState(false);
 	const [hasFirstCallMoreResults, setHasFirstCallMoreResults] = useState(null);
 	const [hasFollowingCallsMoreResults, setHasFollowingCallsMoreResults] = useState(null);
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	const handleSearchQuery = async (event) => {
 		event.preventDefault();
@@ -56,93 +58,89 @@ const Home = (props) => {
 		setSearchResults(null);
 	}
 
-	const toggleMenu = () => {
-		setIsMenuOpen(!isMenuOpen);
-	};
-
 	useEffect(() => {
 		props.latestReleases.next != null ? setHasFirstCallMoreResults(true) : setHasFirstCallMoreResults(false);
 
 		// Check if the browser supports IndexedDB
-		// if(!("indexedDB" in window)) {
-		// 	alert("This browser doesn't support IndexedDB.");
-		// 	return;
-		// }
-
-		// Initialize the database if necessary
-		const openDatabase = window.indexedDB.open("game-world-database", 1);
-
-		openDatabase.onupgradeneeded = (event) => {
-			const database = event.target.result;
-
-			// Create the Games library object store
-			const createGamesLibrary = database.createObjectStore("Games library", {keyPath: "id"});
-
-			// Define the indexes of the Games library
-			createGamesLibrary.createIndex("id", "id", {unique: true});
-			createGamesLibrary.createIndex("slug", "slug", {unique: true});
-			createGamesLibrary.createIndex("as", "as", {unique: true});
-			createGamesLibrary.createIndex("imageSrc", "imageSrc");
-			createGamesLibrary.createIndex("imageAlt", "imageAlt");
-			createGamesLibrary.createIndex("gameName", "gameName");
-			createGamesLibrary.createIndex("gameParentPlatforms", "gameParentPlatforms", {multiEntry: true});
-			createGamesLibrary.createIndex("gamePlatforms", "gamePlatforms", {multiEntry: true});
-			createGamesLibrary.createIndex("gameRelease", "gameRelease");
-			createGamesLibrary.createIndex("gameGenres", "gameGenres", {multiEntry: true});
-
-
-			// Create the Games wishlist object store
-			const createGamesWishlist = database.createObjectStore("Games wishlist", {keyPath: "id"});
-
-			// Define the indexes of the Games wishlist
-			createGamesWishlist.createIndex("id", "id", {unique: true});
-			createGamesWishlist.createIndex("slug", "slug", {unique: true});
-			createGamesWishlist.createIndex("as", "as", {unique: true});
-			createGamesWishlist.createIndex("imageSrc", "imageSrc");
-			createGamesWishlist.createIndex("imageAlt", "imageAlt");
-			createGamesWishlist.createIndex("gameName", "gameName");
-			createGamesWishlist.createIndex("gameParentPlatforms", "gameParentPlatforms", {multiEntry: true});
-			createGamesWishlist.createIndex("gamePlatforms", "gamePlatforms", {multiEntry: true});
-			createGamesWishlist.createIndex("gameRelease", "gameRelease");
-			createGamesWishlist.createIndex("gameGenres", "gameGenres", {multiEntry: true});
-
-
-			// Create the Platforms library object store
-			const createPlatformsLibrary = database.createObjectStore("Platforms library", {keyPath: "id"});
-
-			// Define the indexes of the Platforms library
-			createPlatformsLibrary.createIndex("id", "id", {unique: true});
-			createPlatformsLibrary.createIndex("slug", "slug", {unique: true});
-			createPlatformsLibrary.createIndex("as", "as", {unique: true});
-			createPlatformsLibrary.createIndex("imageSrc", "imageSrc");
-			createPlatformsLibrary.createIndex("imageAlt", "imageAlt");
-			createPlatformsLibrary.createIndex("platformName", "platformName");
-			createPlatformsLibrary.createIndex("gamesCount", "gamesCount", {multiEntry: true});
-			createPlatformsLibrary.createIndex("startYear", "startYear");
-			createPlatformsLibrary.createIndex("endYear", "endYear", {multiEntry: true});
-
-
-			// Create the Platforms wishlist
-			const createPlatformsWishlist = database.createObjectStore("Platforms wishlist", {keyPath: "id"});
-
-			//Define the indexes of the Platform wishlist
-			createPlatformsWishlist.createIndex("id", "id", {unique: true});
-			createPlatformsWishlist.createIndex("slug", "slug", {unique: true});
-			createPlatformsWishlist.createIndex("as", "as", {unique: true});
-			createPlatformsWishlist.createIndex("imageSrc", "imageSrc");
-			createPlatformsWishlist.createIndex("imageAlt", "imageAlt");
-			createPlatformsWishlist.createIndex("platformName", "platformName");
-			createPlatformsWishlist.createIndex("gamesCount", "gamesCount", {multiEntry: true});
-			createPlatformsWishlist.createIndex("startYear", "startYear");
-			createPlatformsWishlist.createIndex("endYear", "endYear", {multiEntry: true});
-		};
-
-		openDatabase.onerror = (event) => {
-			console.error("L'utilisation d'IndexedDB ne semble pas possible. Les fonctions de sauvegarde dans la librairie ne seront pas disponibles.");
+		if(!window.indexedDB) {
+			alert("This browser doesn't support IndexedDB.");
 		}
+		else {
+			// Initialize the database if necessary
+			const openDatabase = window.indexedDB.open("game-world-database", 1);
 
-		openDatabase.onsuccess = (event) => {
-			console.log("La base de données locale a bien été créée/ouverte. Vous pouvez l'utiliser pour enregistrer des objets dans votre librairie.");
+			openDatabase.onupgradeneeded = (event) => {
+				const database = event.target.result;
+
+				// Create the Games library object store
+				const createGamesLibrary = database.createObjectStore("Games library", {keyPath: "id"});
+
+				// Define the indexes of the Games library
+				createGamesLibrary.createIndex("id", "id", {unique: true});
+				createGamesLibrary.createIndex("slug", "slug", {unique: true});
+				createGamesLibrary.createIndex("as", "as", {unique: true});
+				createGamesLibrary.createIndex("imageSrc", "imageSrc");
+				createGamesLibrary.createIndex("imageAlt", "imageAlt");
+				createGamesLibrary.createIndex("gameName", "gameName");
+				createGamesLibrary.createIndex("gameParentPlatforms", "gameParentPlatforms", {multiEntry: true});
+				createGamesLibrary.createIndex("gamePlatforms", "gamePlatforms", {multiEntry: true});
+				createGamesLibrary.createIndex("gameRelease", "gameRelease");
+				createGamesLibrary.createIndex("gameGenres", "gameGenres", {multiEntry: true});
+
+
+				// Create the Games wishlist object store
+				const createGamesWishlist = database.createObjectStore("Games wishlist", {keyPath: "id"});
+
+				// Define the indexes of the Games wishlist
+				createGamesWishlist.createIndex("id", "id", {unique: true});
+				createGamesWishlist.createIndex("slug", "slug", {unique: true});
+				createGamesWishlist.createIndex("as", "as", {unique: true});
+				createGamesWishlist.createIndex("imageSrc", "imageSrc");
+				createGamesWishlist.createIndex("imageAlt", "imageAlt");
+				createGamesWishlist.createIndex("gameName", "gameName");
+				createGamesWishlist.createIndex("gameParentPlatforms", "gameParentPlatforms", {multiEntry: true});
+				createGamesWishlist.createIndex("gamePlatforms", "gamePlatforms", {multiEntry: true});
+				createGamesWishlist.createIndex("gameRelease", "gameRelease");
+				createGamesWishlist.createIndex("gameGenres", "gameGenres", {multiEntry: true});
+
+
+				// Create the Platforms library object store
+				const createPlatformsLibrary = database.createObjectStore("Platforms library", {keyPath: "id"});
+
+				// Define the indexes of the Platforms library
+				createPlatformsLibrary.createIndex("id", "id", {unique: true});
+				createPlatformsLibrary.createIndex("slug", "slug", {unique: true});
+				createPlatformsLibrary.createIndex("as", "as", {unique: true});
+				createPlatformsLibrary.createIndex("imageSrc", "imageSrc");
+				createPlatformsLibrary.createIndex("imageAlt", "imageAlt");
+				createPlatformsLibrary.createIndex("platformName", "platformName");
+				createPlatformsLibrary.createIndex("gamesCount", "gamesCount", {multiEntry: true});
+				createPlatformsLibrary.createIndex("startYear", "startYear");
+				createPlatformsLibrary.createIndex("endYear", "endYear", {multiEntry: true});
+
+
+				// Create the Platforms wishlist
+				const createPlatformsWishlist = database.createObjectStore("Platforms wishlist", {keyPath: "id"});
+
+				//Define the indexes of the Platform wishlist
+				createPlatformsWishlist.createIndex("id", "id", {unique: true});
+				createPlatformsWishlist.createIndex("slug", "slug", {unique: true});
+				createPlatformsWishlist.createIndex("as", "as", {unique: true});
+				createPlatformsWishlist.createIndex("imageSrc", "imageSrc");
+				createPlatformsWishlist.createIndex("imageAlt", "imageAlt");
+				createPlatformsWishlist.createIndex("platformName", "platformName");
+				createPlatformsWishlist.createIndex("gamesCount", "gamesCount", {multiEntry: true});
+				createPlatformsWishlist.createIndex("startYear", "startYear");
+				createPlatformsWishlist.createIndex("endYear", "endYear", {multiEntry: true});
+			};
+
+			openDatabase.onerror = (event) => {
+				console.error("L'utilisation d'IndexedDB ne semble pas possible. Les fonctions de sauvegarde dans la librairie ne seront pas disponibles.");
+			}
+
+			openDatabase.onsuccess = (event) => {
+				console.log("La base de données locale a bien été créée/ouverte. Vous pouvez l'utiliser pour enregistrer des objets dans votre librairie.");
+			}
 		}
 	}, []);
 
