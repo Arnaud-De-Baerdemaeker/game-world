64cef543d89191c423e4e833d5222f2ddda561f2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-10-03 15:11:01 +0200

    [refactor] Rework the component
    - Display an image background in the hero on the detail pages
    - Add the name of the publisher under the name

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,9 +9,28 @@ import heroStyles from "@/components/hero/Hero.module.scss";
 const Hero = (props) => {
 	return(
 		<div className={heroStyles.hero}>
+			{props.imageSrc
+				? (
+					<img
+						src={props.imageSrc}
+						alt={props.imageAlt}
+						className={heroStyles.hero__backgroundImage}
+					/>
+				)
+				: null
+			}
 			<hgroup className={heroStyles.hero__group}>
 				<h2 className={heroStyles.hero__title}>{props.title}</h2>
-				<p className={heroStyles.hero__catchword}>{props.catchword}</p>
+				{typeof props.catchword == "string"
+					? (
+						<p className={heroStyles.hero__catchword}>{props.catchword}</p>
+					)
+					: (
+						props.catchword.map(item => (
+							<p className={heroStyles.hero__catchword}>{item.name}</p>
+						))
+					)
+				}
 			</hgroup>
 		</div>
 	);
